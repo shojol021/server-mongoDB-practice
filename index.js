fefd2258a99cf962c5f8693d0eca8d87f54ed68f
shojol021 <shojol021@gmail.com>
2023-05-11 23:52:11 +0600

    get and read done

--- index.js
+++ index.js
@@ -1,30 +1,64 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
-
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+//middleware
 app.use(cors())
 app.use(express.json());
 
-const users = [
-    { id: 1, name: 'Adnan', email: 'adnan@gmail.com' },
-    { id: 2, name: 'Hossain', email: 'hossain@gmail.com' },
-    { id: 3, name: 'Shojol', email: 'shojol021@gmail.com' }
-]
+// shojol021
+// 9qMJJZuGt6c5SJCV
+
+const uri = "mongodb+srv://shojol021:9qMJJZuGt6c5SJCV@cluster0.6c8obk5.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const userCollection = client.db('userDB').collection('users')
+
+    app.get('/users', async(req, res) => {
+        const cursor =  userCollection.find()
+        const result = await cursor.toArray() 
+        res.send(result)
+    })
+
+    app.post('/users', async(req, res) => {
+        const user = req.body
+        const result = await userCollection.insertOne(user);
+        res.send(result)
+    })
+
+    app.delete('/users/:id', (req, res) => {
+        const id = req.params.id;
+        console.log('please delete', id)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Server is running')
-})
-
-app.get('/users', (req, res) => {
-    res.send(users)
-})
-
-app.post('/users', (req, res) => {
-    const newUser = req.body
-    newUser.id = users.length + 1
-    users.push(newUser)
-    res.send(newUser)
 })
 
 app.listen(port, () => console.log(`running on port ${port}`))
