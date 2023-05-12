c2da276e39863939ebd73f4e6e0bd71cfdd327ca
shojol021 <shojol021@gmail.com>
2023-05-12 11:21:45 +0600

    delete, update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middleware
 app.use(cors())
@@ -35,15 +35,25 @@ async function run() {
         res.send(result)
     })
 
+    app.get('/users/:id', async(req, res) => {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const user = await userCollection.findOne(query)
+      res.send(user);
+    })
+
     app.post('/users', async(req, res) => {
         const user = req.body
         const result = await userCollection.insertOne(user);
         res.send(result)
     })
 
-    app.delete('/users/:id', (req, res) => {
+    app.delete('/users/:id', async (req, res) => {
         const id = req.params.id;
         console.log('please delete', id)
+        const query = {_id: new ObjectId (id)} 
+        const result = await userCollection.deleteOne(query)
+        res.send(result)
     })
 
     // Send a ping to confirm a successful connection
